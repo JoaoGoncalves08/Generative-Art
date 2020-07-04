e5db241c3a9a9e38919da22c2861e0c9c253bf00
JoaoGoncalves08 <joaofranciscogoncalves@tecnico.ulisboa.pt>
2020-07-04 23:42:39 +0100

    added Modern Times 1

---
+++ Modern Times/Modern Times 1/sketch.js
@@ -0,0 +1,77 @@
+var side = 500;
+var s;
+var t = 0;
+var inc = 1;
+
+function setup() {
+	createCanvas(side, side);
+	s = side/6;
+}
+
+function draw() {
+	background(0, 0, 0);
+	noFill();
+
+	translate(s, s);
+
+	//top red
+	stroke(255, 0, 0);
+	fill(255, 0, 0);
+	rect(0, -t, 2*s, s);
+
+	//top magenta
+	stroke(255, 0, 255);
+	fill(255, 0, 255);
+	rect(s - t, 0, 2*s, s);
+	
+	//blue
+	stroke(0, 0, 255);
+	fill(0, 0, 255);
+	rect(2*s + t, 0, s, 2*s);
+
+	//top green
+	stroke(0, 255, 0);
+	fill(0, 255, 0);
+	rect(3*s, -t, s, 2*s);
+
+	//left cyan
+	stroke(0, 255, 255);
+	fill(0, 255, 255);
+	rect(t, s, s, 2*s);
+
+	//yellow
+	stroke(255, 255, 0);
+	fill(255, 255, 0);
+	rect(s, s + t, s, 2*s);
+
+	//white
+	stroke(255, 255, 255);
+	fill(255, 255, 255);
+	rect(0, 3*s - t, 2*s, s);
+
+	//bottom red
+	stroke(255, 0, 0);
+	fill(255, 0, 0);
+	rect(s - 2*t, 2*s, s, 2*s);
+
+	//right cyan
+	stroke(0, 255, 255);
+	fill(0, 255, 255);
+	rect(2*s, 2*s - 2*t, s, 2*s);
+
+	//bottom magenta
+	stroke(255, 0, 255);
+	fill(255, 0, 255);
+	rect(3*s, 2*s + t, s, 2*s);
+
+	//bottom green
+	stroke(0, 255, 0);
+	fill(0, 255, 0);
+	rect(2*s + t, 3*s, 2*s, s);
+
+	t = t + inc;
+
+	if(t > s || t < 0){
+		inc = inc * -1;
+	}
+}
